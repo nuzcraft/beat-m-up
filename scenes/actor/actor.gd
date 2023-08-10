02ff0c7696cb3f9b94c847e5f3d56927b497cf2c
nuzcraft <nuzcraft@gmail.com>
2023-08-09 22:20:25 -0500

    added basic attacks

--- scenes/actor/actor.gd
+++ scenes/actor/actor.gd
@@ -4,10 +4,9 @@ class_name Actor
 @onready var characterSprite: AnimatedSprite2D = $CharacterSprite
 @onready var attackSprite: AnimatedSprite2D = $AttackSprite
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -31,3 +30,12 @@ func take_damage():
 func _on_attack_sprite_animation_finished():
 	if characterSprite.animation == "attack":
 		characterSprite.play("idle")
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "take_damage":
+		die()
+		
+func die():
+	var die_tween = get_tree().create_tween()
+	die_tween.tween_property(characterSprite, "modulate:a", 0, 0.25)
+	die_tween.tween_callback(queue_free)
--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -11,31 +11,42 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_left"):
 		hero.flip_h(false)
-		var enemy = get_closest_enemy(Vector2i(-1, 0))
+		var enemy = get_closest_enemy("left")
 		move_hero_to_attack(enemy)
 	elif Input.is_action_just_pressed("ui_right"):
 		hero.flip_h(true)
-		var enemy = get_closest_enemy(Vector2i(1, 0))
+		var enemy = get_closest_enemy("right")
 		move_hero_to_attack(enemy)
 
-func get_closest_enemy(direction: Vector2):
+func get_closest_enemy(direction: String):
 	var enemies = get_tree().get_nodes_in_group("enemies")
 	if enemies.is_empty():
 		return null
-	var closest = enemies[0]
+	var closest = null
 	for enemy in enemies:
-		var enemy_x = enemy.global_position.x
-		var hero_x = hero.global_position.x
-		var closest_x = closest.global_position.x
-		if abs(enemy_x - hero_x) < abs(closest_x - hero_x) and \
-			enemy_x - hero_x * direction.x > 0:
+		var x_distance = enemy.global_position.x - hero.global_position.x
+		if direction == "right" and x_distance < 0:
+			continue
+		if direction == "left" and x_distance > 0:
+			continue
+		if closest == null:
 			closest = enemy
+		else:
+			if abs(x_distance) < abs(closest.global_position.x - hero.global_position.x):
+				closest = enemy
 	return closest
 	
 func move_hero_to_attack(target):
-	var distance: Vector2 = target.global_position - hero.global_position
-	var normalized: Vector2 = Vector2(distance.x, 0).normalized()
-	hero.global_position = target.global_position
-	hero.global_position.x += (24 * -normalized.x)
-	
+	if target:
+		var distance: Vector2 = target.global_position - hero.global_position
+		var normalized: Vector2 = Vector2(distance.x, 0).normalized()
+		var new_position: Vector2 = Vector2(target.global_position.x + (24 * -normalized.x), target.global_position.y)
+#		hero.global_position = target.global_position
+#		hero.global_position.x += (24 * -normalized.x)
+		var move_hero_tween = get_tree().create_tween()
+		move_hero_tween.set_ease(Tween.EASE_IN_OUT)
+		move_hero_tween.tween_property(hero, "global_position", new_position, 0.1)
+		move_hero_tween.tween_callback(hero.attack)
+		move_hero_tween.tween_callback(target.take_damage)
+#
 		
